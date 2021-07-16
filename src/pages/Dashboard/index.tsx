3b16a62e79a0ccffe4a747473b209790a865592e
Roberta Baseggio <roluiza2017@gmail.com>
2021-07-16 15:24:45 -0300

    Aplicação para salvar as consultas realizadas no local storage

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,7 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight} from 'react-icons/fi';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
 import Repository from '../Repository';
 
 import { Title, Repositories, Form, Error } from './styles';
@@ -17,7 +18,23 @@ interface Repository {
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
   const [inputError, setInputError] = useState('');
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storageRepository = localStorage.getItem(
+      '@GithubExplorer:repositories',
+     );
+
+      if(storageRepository){
+        return JSON.parse(storageRepository);
+      }
+      return [];
+  });
+
+  useEffect(() =>{
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories)
+    )
+  }, [repositories]);
 
   async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
     event.preventDefault();
@@ -56,14 +73,14 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map(repository => (
-          <a key={repository.full_name} href="teste">
+          <Link key={repository.full_name} to={`/repository/${repository.full_name}`}>
             <img src={repository.owner.avatar_url} alt={repository.owner.login} />
             <div>
-            <strong>{repository.full_name}</strong>
-            <p>{repository.description}</p>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
             </div>
             <FiChevronRight size={20}/>
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
